6a6e74dc9fa2e5bfdd585e41d871a7c9bff90d1c
lyp727460067 <727460067@qq.com>
2020-12-28 17:54:33 +0800

    tuning some teb  pra

---
+++ src/mult_point_nav.cpp
@@ -0,0 +1,20 @@
+#include <string>
+#include <vector>
+#include <iostream>
+#include "ros/ros.h"
+#include <ros/>
+
+#include <move_base_msgs/RecoveryStatus.h>
+#include <tf2_geometry_msgs/tf2_geometry_msgs.h>
+#include <move_base/move_base.h>
+
+
+
+
+int main(int argc,char**argv)
+{
+
+
+
+
+}
